334fdd1b84cdd8af93239a3520588f456e64a147
Pedro Lealdini <58371590+Kommnenos@users.noreply.github.com>
2025-06-15 20:39:25 -0300

    Add files via upload

---
+++ src/main/java/br/ufscar/dc/dsw/DCMotors/DcMotorsApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.DCMotors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcMotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcMotorsApplication.class, args);
+	}
+
+}
