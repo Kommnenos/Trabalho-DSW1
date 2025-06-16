9ca9a9198f94601469b1b735a86860437a15d9df
Lucas Velloso <lucas.velloso@dilettasolutions.com>
2025-06-16 16:49:34 -0300

    corrigida localizacao das pastas

--- src/main/java/br/ufscar/dc/dsw/DcMotorsApplication.java
+++ src/main/java/br/ufscar/dc/dsw/DcMotorsApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import br.ufscar.dc.dsw.dao.ILojaDAO;
+import br.ufscar.dc.dsw.dao.IVeiculoDAO;
+import br.ufscar.dc.dsw.dao.IClienteDAO;
+
 @SpringBootApplication(scanBasePackages = "br.ufscar.dc.dsw")
 public class DcMotorsApplication {
 
