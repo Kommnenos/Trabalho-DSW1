1cbb35c1cd8bc3dae5f696dee1e785b44f7183ad
Gabriel Henrique Rodrigues <813345@ufscar.br>
2025-06-16 15:25:17 -0300

    classe cliente

--- db/create.sql
+++ db/create.sql
@@ -2,13 +2,20 @@
 
 -- DROP DATABASE IF EXISTS "DCMotors";
 
-CREATE DATABASE "DCMotors"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en-GB'
-    LC_CTYPE = 'en-GB'
-    LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+-- DATABASE "DCMotors"
+--    WITH
+--    OWNER = postgres
+--    ENCODING = 'UTF8'
+--   LC_COLLATE = 'en-GB'
+ --   LC_CTYPE = 'en-GB'
+ --   LOCALE_PROVIDER = 'libc'
+--    TABLESPACE = pg_default
+ --   CONNECTION LIMIT = -1
+  --  IS_TEMPLATE = False;
+  
+  
+drop database if exists DCMotors;
+
+create database DCMotors;
+
+use DCMotors;
---
+++ src/main/java/br/ufscar/dc/dsw/domain/Cliente.java
@@ -0,0 +1,130 @@
+package br.ufscar.dc.dsw.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+
+
+@Entity
+@Table(name = "Cliente")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@NotBlank
+	@Column(nullable = false, length= 60)
+	private String email;
+	
+	@NotBlank
+	@Column(nullable = false, length= 64)
+	private String senha;
+	
+	@NotBlank
+	@Column(nullable = false, length= 60)
+	private String nome;
+	
+	@NotBlank
+	@Column(nullable = false, length= 11)
+	private String CPF;
+	
+	@Column(nullable = false, length= 15)
+	private String telefone;
+	
+	@Column(nullable = false, length= 1)
+	private String sexo;
+	
+	@Column(nullable = false, length= 20)
+	private LocalDate dataNasc;
+	
+	@Column(nullable = false)
+    private boolean enabled;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public LocalDate getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(LocalDate dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+}
