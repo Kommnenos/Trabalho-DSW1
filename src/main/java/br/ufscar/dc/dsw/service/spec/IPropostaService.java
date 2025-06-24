080ea14c0d112b81c3042bbeef84089adade985f
gabriel <gabrihenriquer@gmail.com>
2025-06-23 22:44:17 -0300

    permitindo que cada loja visualize suas propostas recebidas

--- src/main/java/br/ufscar/dc/dsw/service/spec/IPropostaService.java
+++ src/main/java/br/ufscar/dc/dsw/service/spec/IPropostaService.java
@@ -2,12 +2,14 @@ package br.ufscar.dc.dsw.service.spec;
 
 import java.util.List;
 
+import br.ufscar.dc.dsw.domain.Loja;
 import br.ufscar.dc.dsw.domain.Proposta;
 import br.ufscar.dc.dsw.domain.Cliente;
 
 public interface IPropostaService {
     Proposta buscarPorId(Long id);
     List<Proposta> buscarTodosPorCliente(Cliente cliente);
+    List<Proposta> buscarTodosPorLoja(Loja loja);
     void salvar (Proposta proposta);
     void excluir(Long id);
 }
