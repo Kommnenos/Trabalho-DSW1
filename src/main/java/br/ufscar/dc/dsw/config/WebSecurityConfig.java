73aed8c565c9738f93f300d9b43db68d87ffac4a
gabriel <gabrihenriquer@gmail.com>
2025-06-24 13:30:33 -0300

    apenas adicionando '/' ausentes nas autorizacoes

--- src/main/java/br/ufscar/dc/dsw/config/WebSecurityConfig.java
+++ src/main/java/br/ufscar/dc/dsw/config/WebSecurityConfig.java
@@ -58,9 +58,9 @@ public class WebSecurityConfig {
 						.requestMatchers("/veiculo/cadastrar", "/veiculo/salvar" ).hasRole("LOJA")
 						.requestMatchers("/veiculo/**").hasRole("LOJA")
 						.requestMatchers("/proposta/listar").hasAnyRole("USER","LOJA")
-						.requestMatchers("/proposta/cadastrar/**", "proposta/cliente/**").hasRole("USER")
+						.requestMatchers("/proposta/cadastrar/**", "/proposta/cliente/**").hasRole("USER")
 						.requestMatchers("/proposta/aceitar/**").hasRole("LOJA")
-						.requestMatchers("proposta/loja/**").hasRole("LOJA")
+						.requestMatchers("/proposta/loja/**").hasRole("LOJA")
 						.requestMatchers("/proposta/**" ).hasRole("USER")
 
 						.anyRequest().authenticated())
