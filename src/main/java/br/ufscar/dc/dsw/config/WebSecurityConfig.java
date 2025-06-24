871ac44b3d20609c892a3c834725673be3186a2c
Pedro Lealdini <58371590+Kommnenos@users.noreply.github.com>
2025-06-24 13:08:46 -0300

    fix autorizaçoes

--- src/main/java/br/ufscar/dc/dsw/config/WebSecurityConfig.java
+++ src/main/java/br/ufscar/dc/dsw/config/WebSecurityConfig.java
@@ -59,6 +59,7 @@ public class WebSecurityConfig {
 						.requestMatchers("/veiculo/**").hasRole("LOJA")
 						.requestMatchers("/proposta/listar").hasAnyRole("USER","LOJA")
 						.requestMatchers("/proposta/cadastrar/**", "proposta/cliente/**").hasRole("USER")
+						.requestMatchers("/proposta/aceitar/**").hasRole("LOJA")
 						.requestMatchers("proposta/loja/**").hasRole("LOJA")
 						.requestMatchers("/proposta/**" ).hasRole("USER")
 
