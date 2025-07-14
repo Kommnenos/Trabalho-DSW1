4f27f67477c66a8dd0323e7077b3db83d81afcab
Pedro Lealdini <58371590+Kommnenos@users.noreply.github.com>
2025-07-14 10:16:05 -0300

    boop

--- src/main/java/br/ufscar/dc/dsw/controller/PropostaRestController.java
+++ src/main/java/br/ufscar/dc/dsw/controller/PropostaRestController.java
@@ -9,18 +9,9 @@ import java.math.BigDecimal;
 import br.ufscar.dc.dsw.domain.Proposta;
 import br.ufscar.dc.dsw.domain.Veiculo;
 import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import br.ufscar.dc.dsw.service.spec.IPropostaService;
 import br.ufscar.dc.dsw.domain.StatusProposta;
@@ -28,7 +19,7 @@ import br.ufscar.dc.dsw.domain.Cliente;
 import br.ufscar.dc.dsw.service.spec.IClienteService;
 import br.ufscar.dc.dsw.service.spec.IVeiculoService;
 
-public class PropostaReactController {
+public class PropostaRestController {
 
     private IPropostaService service;
     private IClienteService clienteService;
@@ -88,7 +79,7 @@ public class PropostaReactController {
 
     }
 
-    @GetMapping(path = "/api/cliente/propostas/{id}")
+    @GetMapping(path = "/api/clientes/propostas/{id}")
     public ResponseEntity<List<Proposta>> listaPorCliente(@PathVariable("id") long id) {
         Cliente cliente = clienteService.buscarPorId(id);
         List<Proposta> lista = service.buscarTodosPorCliente(cliente);
@@ -101,7 +92,7 @@ public class PropostaReactController {
 
     }
 
-    @GetMapping(path="/api/veiculo/proposta/{id}")
+    @GetMapping(path="/api/veiculos/proposta/{id}")
     public ResponseEntity<List<Proposta>> listaPorVeiculo(@PathVariable("id") long id){
         Veiculo veiculo = veiculoService.buscarPorId(id);
         List<Proposta> lista = service.buscarTodosPorVeiculo(veiculo);
